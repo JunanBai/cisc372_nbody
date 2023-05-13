5b3837b23db84fb0a5921dc10d99d06509238f9d
Junnan Bai <79231602+JunanBai@users.noreply.github.com>
2023-05-12 20:32:53 -0400

    Update compute.cu

--- compute.cu
+++ compute.cu
@@ -1,61 +1,90 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <cuda.h>
+#include <math.h>
 #include "vector.h"
-#include "compute.h"
 #include "config.h"
 
+#define n NUMENTITIES
 #define BLOCK_SIZE 256
 
-__global__ void computeForces(int n, vector3 *pos, vector3 *vel, double *mass, vector3 *force) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) {
-        int j;
-        vector3 f = {0.0, 0.0, 0.0};
-        for (j = 0; j < n; j++) {
-            if (i != j) {
-                double dx = pos[j].x - pos[i].x;
-                double dy = pos[j].y - pos[i].y;
-                double dz = pos[j].z - pos[i].z;
-                double dist = sqrt(dx * dx + dy * dy + dz * dz);
-                double mag = GRAV_CONSTANT * *mass[i] * *mass[j] / (dist * dist * dist);
-                f.x += mag * dx;
-                f.y += mag * dy;
-                f.z += mag * dz;
+// CUDA kernel for calculating pairwise accelerations
+__global__ void calculate_accelerations(vector3 *dPos, vector3 *dAccels, double *dMass) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if (i < NUMENTITIES && j < NUMENTITIES) {
+        if (i != j) {
+            vector3 dist;
+            for (int k = 0; k < 3; ++k) {
+                dist[k] = dPos[j][k] - dPos[i][k];
+            }
+
+            double dist_sq = dist[0]*dist[0] + dist[1]*dist[1] + dist[2]*dist[2];
+            double dist_mag = sqrt(dist_sq);
+            double accel_mag = GRAV_CONSTANT * dMass[j] / (dist_sq * dist_mag);
+
+            for (int k = 0; k < 3; ++k) {
+                dAccels[i*NUMENTITIES+j][k] = accel_mag * dist[k];
+            }
+        } else {
+            for (int k = 0; k < 3; ++k) {
+                dAccels[i*NUMENTITIES+j][k] = 0.0;
             }
         }
-        force[i] = f;
     }
 }
 
-__global__ void computeAcceleration(int n, vector3 *pos, vector3 *vel, double *mass, vector3 *force, vector3 *acc) {
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n) {
-        acc[i].x = force[i].x / *mass[i];
-        acc[i].y = force[i].y / *mass[i];
-        acc[i].z = force[i].z / *mass[i];
+// CUDA kernel for calculating new velocities and positions
+__global__ void calculate_velocities_positions(vector3 *dPos, vector3 *dVel, double *dMass) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    vector3 accel_sum = {0, 0, 0};
+    vector3 *dAccels;
+    if (i < NUMENTITIES) {
+        for (int j = 0; j < NUMENTITIES; ++j) {
+            for (int k = 0; k < 3; ++k) {
+                accel_sum[k] += dAccels[i*NUMENTITIES+j][k];
+            }
+        }
+
+        for (int k = 0; k < 3; ++k) {
+            dVel[i][k] += accel_sum[k] * INTERVAL;
+            dPos[i][k] += dVel[i][k] * INTERVAL;
+        }
     }
 }
 
-void compute(int n, vector3 *pos, vector3 *vel, double *mass, vector3 *acc) {
+void compute(vector3 *hPos, vector3 *hVel, double *mass){
+    // Allocate memory on device
     int numBlocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
-    vector3 *d_pos, *d_vel, *d_force, *d_acc;
-    double *d_mass;
-    cudaMalloc(&d_pos, n * sizeof(vector3));
-    cudaMalloc(&d_vel, n * sizeof(vector3));
-    cudaMalloc(&d_mass, n * sizeof(double));
-    cudaMalloc(&d_force, n * sizeof(vector3));
-    cudaMalloc(&d_acc, n * sizeof(vector3));
-    cudaMemcpy(d_pos, pos, n * sizeof(vector3), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_vel, vel, n * sizeof(vector3), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_mass, mass, n * sizeof(double), cudaMemcpyHostToDevice);
-    computeForces<<<numBlocks, BLOCK_SIZE>>>(n, d_pos, d_vel, d_mass, d_force);
-    computeAcceleration<<<numBlocks, BLOCK_SIZE>>>(n, d_pos, d_vel, d_mass, d_force, d_acc);
-    cudaMemcpy(acc, d_acc, n * sizeof(vector3), cudaMemcpyDeviceToHost);
-    cudaFree(d_pos);
-    cudaFree(d_vel);
-    cudaFree(d_mass);
-    cudaFree(d_force);
-    cudaFree(d_acc);
+    dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
+    vector3 *dPos, *dVel, *dAccels;
+    double *dMass;
+    size_t size = NUMENTITIES * sizeof(vector3);
+    size_t sizeMass = NUMENTITIES * sizeof(double);
+    size_t sizeAccels = NUMENTITIES * NUMENTITIES * sizeof(vector3);
+
+    cudaMalloc(&dPos, size);
+    cudaMalloc(&dVel, size);
+    cudaMalloc(&dMass, sizeMass);
+    cudaMalloc(&dAccels, sizeAccels);
+
+    cudaMemcpy(dPos, hPos, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dVel, hVel, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dMass, mass, sizeMass, cudaMemcpyHostToDevice);
+
+    calculate_accelerations<<<numBlocks, threadsPerBlock>>>(dPos, dAccels, dMass);
+    cudaDeviceSynchronize();  // Wait for the GPU to finish before accessing on host
+
+    calculate_velocities_positions<<<numBlocks, threadsPerBlock>>>(dPos, dVel, dMass);
+    cudaDeviceSynchronize();  
+
+    cudaMemcpy(hPos, dPos, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(hVel, dVel, size, cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(dPos);
+    cudaFree(dVel);
+    cudaFree(dMass);
+    cudaFree(dAccels);
 }
+
