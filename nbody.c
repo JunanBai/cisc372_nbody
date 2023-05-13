d917ae2d244b8e96cbbb5a9aa86fe5c03af03e3a
Junnan Bai <79231602+JunanBai@users.noreply.github.com>
2023-05-12 20:29:28 -0400

    Update nbody.c

--- nbody.c
+++ nbody.c
@@ -1,117 +1,61 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 #include <math.h>
+#include <cuda.h>
 #include "vector.h"
-#include "config.h"
-#include "planets.h"
 #include "compute.h"
-#include "cuda.h"
-#include "cuda_runtime.h"
+#include "config.h"
 
-// represents the objects in the system.  Global variables
-vector3 *hVel, *d_hVel;
-vector3 *hPos, *d_hPos;
-double *mass;
+#define BLOCK_SIZE 256
 
-//initHostMemory: Create storage for numObjects entities in our system
-//Parameters: numObjects: number of objects to allocate
-//Returns: None
-//Side Effects: Allocates memory in the hVel, hPos, and mass global variables
-void initHostMemory(int numObjects)
-{
-	hVel = (vector3 *)malloc(sizeof(vector3) * numObjects);
-	hPos = (vector3 *)malloc(sizeof(vector3) * numObjects);
-	mass = (double *)malloc(sizeof(double) * numObjects);
+__global__ void computeForces(int n, vector3 *pos, vector3 *vel, double *mass, vector3 *force) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        int j;
+        vector3 f = {0.0, 0.0, 0.0};
+        for (j = 0; j < n; j++) {
+            if (i != j) {
+                double dx = pos[j].x - pos[i].x;
+                double dy = pos[j].y - pos[i].y;
+                double dz = pos[j].z - pos[i].z;
+                double dist = sqrt(dx * dx + dy * dy + dz * dz);
+                double mag = GRAV_CONSTANT * *mass[i] * *mass[j] / (dist * dist * dist);
+                f.x += mag * dx;
+                f.y += mag * dy;
+                f.z += mag * dz;
+            }
+        }
+        force[i] = f;
+    }
 }
 
-//freeHostMemory: Free storage allocated by a previous call to initHostMemory
-//Parameters: None
-//Returns: None
-//Side Effects: Frees the memory allocated to global variables hVel, hPos, and mass.
-void freeHostMemory()
-{
-	free(hVel);
-	free(hPos);
-	free(mass);
+__global__ void computeAcceleration(int n, vector3 *pos, vector3 *vel, double *mass, vector3 *force, vector3 *acc) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        acc[i].x = force[i].x / *mass[i];
+        acc[i].y = force[i].y / *mass[i];
+        acc[i].z = force[i].z / *mass[i];
+    }
 }
 
-//planetFill: Fill the first NUMPLANETS+1 entries of the entity arrays with an estimation
-//				of our solar system (Sun+NUMPLANETS)
-//Parameters: None
-//Returns: None
-//Fills the first 8 entries of our system with an estimation of the sun plus our 8 planets.
-void planetFill(){
-	int i,j;
-	double data[][7]={SUN,MERCURY,VENUS,EARTH,MARS,JUPITER,SATURN,URANUS,NEPTUNE};
-	for (i=0;i<=NUMPLANETS;i++){
-		for (j=0;j<3;j++){
-			hPos[i][j]=data[i][j];
-			hVel[i][j]=data[i][j+3];
-		}
-		mass[i]=data[i][6];
-	}
-}
-
-//randomFill: FIll the rest of the objects in the system randomly starting at some entry in the list
-//Parameters: 	start: The index of the first open entry in our system (after planetFill).
-//				count: The number of random objects to put into our system
-//Returns: None
-//Side Effects: Fills count entries in our system starting at index start (0 based)
-void randomFill(int start, int count)
-{
-	int i, j, c = start;
-	for (i = start; i < start + count; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			hVel[i][j] = (double)rand() / RAND_MAX * MAX_DISTANCE * 2 - MAX_DISTANCE;
-			hPos[i][j] = (double)rand() / RAND_MAX * MAX_VELOCITY * 2 - MAX_VELOCITY;
-			mass[i] = (double)rand() / RAND_MAX * MAX_MASS;
-		}
-	}
-}
-
-//printSystem: Prints out the entire system to the supplied file
-//Parameters: 	handle: A handle to an open file with write access to prnt the data to
-//Returns: 		none
-//Side Effects: Modifies the file handle by writing to it.
-void printSystem(FILE* handle){
-	int i,j;
-	for (i=0;i<NUMENTITIES;i++){
-		fprintf(handle,"pos=(");
-		for (j=0;j<3;j++){
-			fprintf(handle,"%lf,",hPos[i][j]);
-		}
-		printf("),v=(");
-		for (j=0;j<3;j++){
-			fprintf(handle,"%lf,",hVel[i][j]);
-		}
-		fprintf(handle,"),m=%lf\n",mass[i]);
-	}
-}
-
-int main(int argc, char **argv)
-{
-	clock_t t0=clock();
-	int t_now;
-	//srand(time(NULL));
-	srand(1234);
-	initHostMemory(NUMENTITIES);
-	planetFill();
-	randomFill(NUMPLANETS + 1, NUMASTEROIDS);
-	//now we have a system.
-	#ifdef DEBUG
-	printSystem(stdout);
-	#endif
-	for (t_now=0;t_now<DURATION;t_now+=INTERVAL){
-		compute();
-	}
-	clock_t t1=clock()-t0;
-#ifdef DEBUG
-	printSystem(stdout);
-#endif
-	printf("This took a total time of %f seconds\n",(double)t1/CLOCKS_PER_SEC);
-
-	freeHostMemory();
+void compute(int n, vector3 *pos, vector3 *vel, double *mass, vector3 *acc) {
+    int numBlocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    vector3 *d_pos, *d_vel, *d_force, *d_acc;
+    double *d_mass;
+    cudaMalloc(&d_pos, n * sizeof(vector3));
+    cudaMalloc(&d_vel, n * sizeof(vector3));
+    cudaMalloc(&d_mass, n * sizeof(double));
+    cudaMalloc(&d_force, n * sizeof(vector3));
+    cudaMalloc(&d_acc, n * sizeof(vector3));
+    cudaMemcpy(d_pos, pos, n * sizeof(vector3), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_vel, vel, n * sizeof(vector3), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_mass, mass, n * sizeof(double), cudaMemcpyHostToDevice);
+    computeForces<<<numBlocks, BLOCK_SIZE>>>(n, d_pos, d_vel, d_mass, d_force);
+    computeAcceleration<<<numBlocks, BLOCK_SIZE>>>(n, d_pos, d_vel, d_mass, d_force, d_acc);
+    cudaMemcpy(acc, d_acc, n * sizeof(vector3), cudaMemcpyDeviceToHost);
+    cudaFree(d_pos);
+    cudaFree(d_vel);
+    cudaFree(d_mass);
+    cudaFree(d_force);
+    cudaFree(d_acc);
 }
